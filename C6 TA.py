8ebe51e8f7c1c3dd927d4688de3a2ea7ac681f30
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-10-20 11:12:23 +0530

    Update C6 TA.py

--- C6 TA.py
+++ C6 TA.py
@@ -4,7 +4,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 400))
 
-# create a variable game_state and assign value "play" to it.
+#Create a variable game_state and assign value "play" to it.
 
 score = 0
 score_font = pygame.font.Font("freesansbold.ttf", 16)
@@ -21,13 +21,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             
+        #Add play state here. Indent the following 6 lines. This will make the keys work for gameplay.
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     dino_y_change = -1
         if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
                     dino_y_change = 1
+        
+        #Add the over state code block here. This will make the keys run for restarting the game.
+            #if event.type == pygame.KEYDOWN:
+                #if event.key == pygame.K_SPACE:
+                    #Reset game_state = "play"
+                    #Reset cactus_rect.x = 1200
+                    #Reset score = 0
+                    #Reset dino_y_change = 1
     
+    #Add play game_state. Indent the next 22 lines. This will ensure that the code indented in this block runs only when game is in play state.
     dino_rect.y += dino_y_change
     if dino_rect.y > 250:
         dino_rect.y = 250
@@ -49,7 +59,11 @@ while True:
     
     if dino_rect.colliderect(cactus_rect):
         pygame.time.delay(2000)
-        pygame.quit()
+        pygame.quit()   #Change this line to set game_state to over.
+        
+    #Add the over state code block here. This will ensure that the code indented in this block runs only when game is in over state.
+        #pygame.time.delay(500)
+        #game_state = "over"
     
     pygame.display.update()
     
